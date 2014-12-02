05dc8a9d3bb6a6c03a0a6c5c3556b75595b44dd3
James Elías <jamiees2@gmail.com>
2014-12-02 14:08:55 +0000

    Added stuff

--- consoleui.cpp
+++ consoleui.cpp
@@ -214,7 +214,7 @@ vector<Scientist> ConsoleUI::search(){
          << "\tDeathdate (Fuzzy) (10)" << endl
          << "\tNationality (Fuzzy) (11)" << endl;
     do{
-        cout << "What would you like to search by? (Default 1): ";
+        cout << "What would you like to  by? (Default 1): ";
         if(readline(ss))
             ss >> field;
         else
@@ -222,7 +222,7 @@ vector<Scientist> ConsoleUI::search(){
     } while(field <= 0 || field > 12);
     if (field > 6) {
         if (field > 8) field++; // Shift before the modulo operation
-        field = field % 7; // Bound the fuzzy choices
+        field = (field % 6) + 1; // Bound the fuzzy choices
         fuzzy = true;
     }
     cout << "What is the maximum number of entries you want? (Default 1)";
@@ -230,10 +230,10 @@ vector<Scientist> ConsoleUI::search(){
         ss >> rows;
     vector<Scientist> vec;
     if(rows <= 0)
-        return vec; // Why go through a search if the user doesnt want results?
+        return vec; // Why go through a  if the user doesnt want results?
     cout << "Enter your query: ";
     getline(cin, query);
-    vec = scientistService.search(static_cast<ScientistSort::Field>(field), fuzzy, rows, query);
+    vec = scientistService.(static_cast<ScientistSort::Field>(field), fuzzy, rows, query);
     header();
     for(size_t i = 0; i<vec.size(); i++){
         cout << left 
