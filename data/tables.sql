d0cde850936faf1c5e2eca6c151a0a3c2b5ad53c
James Elías <jamiees2@gmail.com>
2014-12-04 00:14:24 +0000

    reformatted sql file

--- data/tables.sql
+++ data/tables.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS scientists (
         id integer primary key,
         first_name varchar(255),
         last_name varchar(255),
-        gender char(1),
+        gender varchar(1),
         birth_date date,
         death_date date,
-        nationality varchar(255
-));
+        nationality varchar(255)
+);
