d1efecf6ae6413e7871bda19cc41c2305e452c6b
Ari Thorlacius <arithorlacius@gmail.com>
2014-12-04 14:23:53 +0000

    Fixed computer class

--- computer.cpp
+++ computer.cpp
@@ -3,3 +3,22 @@
 Computer::Computer()
 {
 }
+
+Computer Computer::fromQuery(const QSqlQuery* query){
+    Computer comp;
+    comp.id = query->value("id").toInt();
+    comp.name = query->value("name").toString().toStdString();
+    comp.type = query->value("type").toString().toStdString();
+    comp.buildyear = query->value("build_year").toInt();
+    comp.built = query->value("was_built").toBool();
+    return comp;
+}
+
+bool operator ==(const Computer &a, const Computer &b){
+    if(a.name != b.name) return false;
+    else if (a.type != b.type) return false;
+    else if (a.buildyear != b.buildyear) return false;
+    else if (a.built != b.built) return false;
+
+    return true;
+}
--- computer.h
+++ computer.h
@@ -1,10 +1,27 @@
 #ifndef COMPUTER_H
 #define COMPUTER_H
 
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <QSqlQuery>
+#include <QVariant>
+#include <QChar>
+
+using namespace std;
+
 class Computer
+
 {
 public:
     Computer();
+    int id;
+    string name;
+    string type;
+    int buildyear;
+    bool built;
+
+    static Computer fromQuery(const QSqlQuery* query);
+    friend bool operator == (const Computer &a, const Computer &b);
 };
 
-#endif // COMPUTER_H
