fe10c7b87d228f5d8285caa548ab3fe9755cd61a
James Elías <jamiees2@gmail.com>
2014-12-09 22:38:11 +0000

    Added data

---
+++ data.sql
@@ -0,0 +1,20 @@
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Blaise','Pascal','M','19.06.1623','19.08.1662','French');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Alan','Turing','M','23.06.1912','07.06.1954','English');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Daniel','Brandur','M','23.07.1987','22.06.1987','Icelandic');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Charles','Babbage','M','26.12.1791','18.10.1871','English');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Edsger','Djikstra','M','11.05.1930','06.08.2002','Dutch');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Chuck','Norris','M','10.03.1940','','American');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Ada','Lovelace','F','10.12.1815','27.11.1852','English');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Leonhard','Euler','M','15.04.1707','18.09.1783','Swiss');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Donald','Knuth','M','10.01.1938', '','American');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Tim','Berners-Lee','M','08.06.1955', '','English');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Larry','Page','M','26.03.1973', '','American');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Sergey','Brin','M','21.08.1973', '','American');
+
+
+INSERT INTO computers(name, build_year, type, built) VALUES('Pascaline',1,1642, 'Mechanical',1);
+INSERT INTO computers(name, build_year, type, built) VALUES('ABC',1942, 'Electronic',1);
+INSERT INTO computers(name, build_year, type, built) VALUES('Colossus',1943, 'Electronic',1);
+INSERT INTO computers(name, build_year, type, built) VALUES('Stepped Reckoner',1672, 'Mechanical',1);
+INSERT INTO computers(name, build_year, type, built) VALUES('Difference Engine',1822, 'Mechanical',1);
+INSERT INTO computers(name, build_year, type, built) VALUES('Analytical Engine',1837, 'Mechanical',1);
