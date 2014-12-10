0463d1991ce13c779fd945ee161e89b51907bedd
saraarnad <saraarn13@ru.is>
2014-12-10 00:47:12 +0000

    Push baby push!!

--- data.sql
+++ data.sql
@@ -10,6 +10,9 @@ INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, na
 INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Tim','Berners-Lee','M','1955-06-08', '','English');
 INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Larry','Page','M','1973-03-26', '','American');
 INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Sergey','Brin','M','1973-08-21', '','American');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Steve','Wozniak','M','1950-08-11', '','American');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('John','Backus','M','1924-12-03', '2007-03-17','American');
+INSERT INTO scientists(first_name, last_name, gender, birth_date, death_date, nationality) VALUES ('Tony','Hoare','M','1934-01-11', '','British');
 
 
 INSERT INTO computers(name, build_year, type, built) VALUES('Pascaline',1642, 'Mechanical',1);
@@ -18,3 +21,5 @@ INSERT INTO computers(name, build_year, type, built) VALUES('Colossus',1943, 'El
 INSERT INTO computers(name, build_year, type, built) VALUES('Stepped Reckoner',1672, 'Mechanical',1);
 INSERT INTO computers(name, build_year, type, built) VALUES('Difference Engine',1822, 'Mechanical',1);
 INSERT INTO computers(name, build_year, type, built) VALUES('Analytical Engine',1837, 'Mechanical',1);
+INSERT INTO computers(name, build_year, type, built) VALUES('Apple 1',1976, 'Electronic',1);
+INSERT INTO computers(name, build_year, type, built) VALUES('ENIAC',1946, 'Electornic',1);
